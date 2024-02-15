5c287230310a084e2c89ad2bb71c6d12acbe52db
EvoNexusX <139948124+EvoNexusX@users.noreply.github.com>
2024-02-15 17:10:05 +0800

    Add files via upload

---
+++ NECSO/Niching_Competition.m
@@ -0,0 +1,24 @@
+function [new_velocity, new_position, flag] = Niching_Competition(particles, i, groups,phi)
+    [group_particles,~] = find_particle_group(groups,i);
+    group_mean_position = mean(particles.pos(group_particles, :));
+    
+    random_index = randperm(length(group_particles), 1);
+    competitor_index = group_particles(random_index);
+    
+    Dim = length(particles.pos(1,:));
+    if particles.fitness(i) > particles.fitness(competitor_index)
+        % Update velocity and position
+        R1 = rand(1,Dim);
+        R2 = rand(1,Dim);
+        R3 = rand(1,Dim);
+        new_velocity = R1.*particles.vel(i, :) + R2.*(particles.pos(competitor_index, :)-particles.pos(i, :))... 
+        + phi.*R3.*(group_mean_position-particles.pos(i, :));
+        new_position = particles.pos(i, :) + new_velocity;
+        flag = 1;
+    else
+        % No update if particle is inferior
+        new_velocity = particles.vel(i, :);
+        new_position = particles.pos(i, :);
+        flag = 0;
+    end
+end
